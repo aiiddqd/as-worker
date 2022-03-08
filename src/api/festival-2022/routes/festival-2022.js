d2cc8d62e3fb5b0894eed4e5bac81673721a10d7
uptimizt <uptimizt@gmail.com>
2022-03-08 10:50:15 +0300

    .

---
+++ src/api/festival-2022/routes/festival-2022.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * festival-2022 router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::festival-2022.festival-2022');
---
+++ src/api/festival-2022/services/festival-2022.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * festival-2022 service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::festival-2022.festival-2022');
