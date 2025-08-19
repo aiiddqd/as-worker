6865d4f436ad3c33370063f68a093b875e1dd9ea
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-08-19 09:04:39 +0300

    вв

--- main.php
+++ main.php
@@ -25,81 +25,82 @@ class Plugin
 
 
             if (class_exists('WP_CLI')) {
-
-
-                WP_CLI::add_command('as-worker', function ($argv, $assoc_args) {
-
-                    if (isset($argv[0]) && 'stop' === $argv[0]) {
-                        set_transient('as-worker-hard-stop', true, MINUTE_IN_SECONDS * 30);
-                        return true;
-                    }
-
-                    $is_active = get_transient('as-worker');
-                    if ($is_active) {
-                        WP_CLI::log('as-worker - already running');
-                        return false;
-                    }
-
-                    $couters = [
-                        'total' => 0,
-                        'iterations' => 0
-                    ];
-                    while (true) {
-
-                        $couters['iterations']++;
-                        try {
-
-                            $is_active = get_transient('as-worker');
-
-                            if (get_transient('as-worker-hard-stop')) {
-                                delete_transient('as-worker-hard-stop');
-                                WP_CLI::log('as-worker-hard-stop');
-                                break;
-                            };
-
-                            if ($is_active) {
-
-                                // if working longer than 1 hour - stop 
-                                if (time() - $is_active > 60 * 60) {
-                                    delete_transient('as-worker');
-                                    WP_CLI::log('if working longer than 1 hour - stop');
-                                    return;
-                                }
-                            } else {
-                                set_transient('as-worker', time(), HOUR_IN_SECONDS);
-                            }
-
-                            $jobsNumber = ActionScheduler_QueueRunner::instance()->run('WP CLI AS Worker');
-                            do_action('as_worker_iteration');
-                            $couters['rows'] = $jobsNumber;
-                            $couters['total'] += $jobsNumber;
-
-                            WP_CLI::log('Jobs: '.print_r($couters, true));
-
-                            if (empty($jobsNumber)) {
-                                WP_CLI::log('Jobs - auto stop if empty');
-                                do_action('as_worker_iteration_empty', $couters);
-                                delete_transient('as-worker');
-                                break;
-                            } else {
-                                wc_get_logger()->info('Jobs - doing iteration', [
-                                    'source' => 'as-worker',
-                                    'couters' => $couters,
-                                ]);
-                            }
-
-                        } catch (\Throwable $th) {
-                            wc_get_logger()->error($th->getMessage().'... '.print_r($couters, true), ['source' => 'as_worker']);
-                            delete_transient('as-worker');
-                            do_action('as_worker_iteration_error', $th);
-                            break;
-                        }
-                    }
-
-
-                });
+                WP_CLI::add_command('as-worker', [self::class, 'handler']);
             }
         });
     }
-}
 
+    public static function handler($argv, $assoc_args)
+    {
+        // Handle the worker logic here
+
+        if (isset($argv[0]) && 'stop' === $argv[0]) {
+            set_transient('as-worker-hard-stop', true, MINUTE_IN_SECONDS * 30);
+            return true;
+        }
+
+        $is_active = get_transient('as-worker');
+        if ($is_active) {
+            WP_CLI::log('as-worker - already running');
+            return false;
+        }
+
+        $couters = [
+            'total' => 0,
+            'iterations' => 0
+        ];
+        while (true) {
+
+            if (get_transient('as-worker-hard-stop')) {
+                delete_transient('as-worker-hard-stop');
+                WP_CLI::log('as-worker-hard-stop');
+                break;
+            }
+            ;
+
+            $couters['iterations']++;
+
+            try {
+
+                $is_active = get_transient('as-worker');
+
+                if ($is_active) {
+
+                    // if working longer than 1 hour - stop 
+                    if (time() - $is_active > 60 * 60) {
+                        delete_transient('as-worker');
+                        WP_CLI::log('if working longer than 1 hour - stop');
+                        return;
+                    }
+                } else {
+                    set_transient('as-worker', time(), HOUR_IN_SECONDS);
+                }
+
+                $jobsNumber = ActionScheduler_QueueRunner::instance()->run('WP CLI AS Worker');
+                do_action('as_worker_iteration');
+                $couters['rows'] = $jobsNumber;
+                $couters['total'] += $jobsNumber;
+
+                WP_CLI::log('Jobs: '.print_r($couters, true));
+
+                if (empty($jobsNumber)) {
+                    WP_CLI::log('Jobs - auto stop if empty');
+                    do_action('as_worker_iteration_empty', $couters);
+                    delete_transient('as-worker');
+                    break;
+                } else {
+                    wc_get_logger()->info('Jobs - doing iteration', [
+                        'source' => 'as-worker',
+                        'couters' => $couters,
+                    ]);
+                }
+
+            } catch (\Throwable $th) {
+                wc_get_logger()->error($th->getMessage().'... '.print_r($couters, true), ['source' => 'as_worker']);
+                delete_transient('as-worker');
+                do_action('as_worker_iteration_error', $th);
+                break;
+            }
+        }
+    }
+}
